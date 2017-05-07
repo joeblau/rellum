4f087b00583782a7ab93e339a685acc0915b8bee
Joe Blau <josephblau@gmail.com>
2017-05-06 21:06:51 -0400

    Initial Commit

---
+++ Rellum/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Rellum
+//
+//  Created by Joseph Blau on 5/6/17.
+//  Copyright © 2017 Design Utilities. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
